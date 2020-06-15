bb96f078ca8ca45105468e7124999663704856f0
alan <alanarostegui@hotmail.com>
2020-06-14 21:51:16 -0500

    initial commit

---
+++ escuela/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace escuela
+{
+    class Escuela
+    {
+        public string nombre;
+        public string direccion;
+        public int añoFundacion;
+
+        public string ceo;
+
+        public void Timbrar()
+        {
+            // to do
+            Console.Beep(2000, 3000);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var miEscuela = new Escuela();
+            miEscuela.nombre = "DevF";
+            miEscuela.direccion = "Una direccion random";
+            miEscuela.añoFundacion = 2014;
+            Console.WriteLine("Timbrando");
+            miEscuela.Timbrar();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ primer_programa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fundamentos_de_C__con_NET_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
